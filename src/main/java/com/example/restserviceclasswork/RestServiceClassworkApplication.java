b459e400163641aaca209d139bcedd2af5d72c2e
MTBakalov18 <MTBakalov18@codingburgas.bg>
2022-11-02 15:35:53 +0200

    Commit

---
+++ src/main/java/com/example/restserviceclasswork/RestServiceClassworkApplication.java
@@ -0,0 +1,13 @@
+package com.example.restserviceclasswork;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestServiceClassworkApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RestServiceClassworkApplication.class, args);
+    }
+
+}
